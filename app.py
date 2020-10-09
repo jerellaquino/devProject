5d64f4a9c181d8864acbf24c0bbb23c23319c1b5
jerellaquino <70453384+jerellaquino@users.noreply.github.com>
2020-10-08 21:19:40 -0400

    removed comment

--- app.py
+++ app.py
@@ -3,18 +3,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def helloWorld():
     return render_template("index.html")
-
-
 
 @app.route('/hello/')
 @app.route('/hello/<name>')
 def helloName(name=None):
     return render_template("hello.html", name=name)
-
 
 
 #Read parks.json file and store it as parks
@@ -26,13 +22,7 @@ with open('parks.json', 'r') as f:
 def parksList():
     return render_template("parksTemplate.html", parks = parks)
 
-"""
-@app.route("/content")
-def my_route():
-  content = {'thing':'some stuff',
-             'other':'more stuff'}
-  return render_template('parksTemplate.html', **content)
-"""
+
 if __name__ == "__main__":
     app.run()
 
